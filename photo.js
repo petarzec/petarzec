c1c18c7295e44dab5ad9c6fa8d07d01e2c44da3e
petarzec <petarzec07@gmail.com>
2026-04-24 00:34:56 -0700

    1.0

---
+++ photo.js
@@ -0,0 +1,14 @@
+const photos = [
+    { url: "https://picsum.photos/id/10/800/1000", caption: "Mountain dawn" },
+    { url: "https://picsum.photos/id/29/800/600", caption: "Forest path" },
+    { url: "https://picsum.photos/id/37/800/1000", caption: "Coastal rocks" },
+    { url: "https://picsum.photos/id/42/800/600", caption: "Desert light" },
+    { url: "https://picsum.photos/id/59/800/1000", caption: "River bend" },
+    { url: "https://picsum.photos/id/64/800/600", caption: "Urban geometry" },
+    { url: "https://picsum.photos/id/76/800/1000", caption: "Misty valley" },
+    { url: "https://picsum.photos/id/84/800/600", caption: "Golden hour" },
+    { url: "https://picsum.photos/id/96/800/1000", caption: "Still waters" },
+    { url: "https://picsum.photos/id/103/800/600", caption: "Stone bridge" },
+    { url: "https://picsum.photos/id/119/800/1000", caption: "Autumn road" },
+    { url: "https://picsum.photos/id/125/800/600", caption: "Snowy peaks" },
+];
